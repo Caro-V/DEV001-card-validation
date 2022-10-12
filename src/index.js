da025c39d706b4c56e091cec978970be307eaa7e
Caro-V <carolinavelasquez@MacBook-Air-de-Carolina.local>
2022-10-12 12:24:41 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import validator from './validator.js';
 
-console.log(validator);
+//Al click en el btn1 se debe mostrar la sección Payment y ocultar la sección Purchase
+document.getElementById("btn1").addEventListener("click", function() {
+    document.getElementById("purchase").className = "hide";
+    document.getElementById("payment").className = "show";
+    document.getElementById("confirmation").className = "hide";
+});
+
+
+
+const card = document.getElementById("card_number")
+const button =  document.getElementById("btn2")
+//const expDate = document.getElementById("exp_Date")
+//const cvv = document.getElementById("cvv")
+
+
+
+
+    button.addEventListener("click", function() {
+        document.getElementById("purchase").className = "hide";
+        document.getElementById("payment").className = "hide";
+        document.getElementById("confirmation").className = "show";
+
+        if (card.value === ''){
+            document.getElementById("warning").innerHTML = "Debe ingresar el número de tarjeta";
+            
+        } else {
+            let finalMaskify = validator.maskify(card.value)
+            let finalValidation = validator.isValid(card.value)
+            if (finalValidation == true) {
+                document.getElementById("warning").innerHTML= "La tarjeta " + finalMaskify + " es VÁLIDA.<br> Recibirá un correo electrónico con su número de seguimiento."
+            } else {
+                document.getElementById("warning").innerHTML= "La tarjeta " + finalMaskify + " es INVÁLIDA. <br> Ingrese otra tarjeta."
+            }
+
+        }
+    })
+
+    
+
+
+console.log(validator)
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,64 @@
 const validator = {
-  // ...
+
+
+  isValid (creditCardNumber) {
+
+    //Traer el input a un array
+    let cardArray = creditCardNumber.split("")
+    let addDigits = 0
+    let totalEven = 0
+    let totalOdd = 0
+    
+
+    //Ciclo for para cada índice del arreglo según se encuentre en una posición par o impar
+    for (let i = 0; i < cardArray.length; i++ ){      
+      let numPosition = Number(cardArray[i])
+      
+      //Duplicar los números que están en las posiciones pares
+      if (i % 2 === 0) {
+        numPosition = cardArray[i]*2
+
+            //Sumar los dígitos de los números mayores o iguales a diez
+            if  (numPosition >= 10){
+              addDigits = numPosition - 9
+              totalEven += addDigits
+              //console.log(addDigits + " suma de los mayores a diez")
+            } else {totalEven += numPosition}   
+
+      } 
+      //Números en posiciones impares
+      else { 
+        numPosition = Number(cardArray[i])
+        totalOdd += numPosition
+        }
+      }
+        
+      //Variable con la suma de los números en posicioes pares e impares
+      let totalTotal = totalEven + totalOdd
+
+      //Si el total dividido entre diez tiene residuo 0, indicar si es VÁLIDA o INVÁLIDA
+      if (totalTotal % 10 === 0) {
+      return true
+      } else {
+      return false 
+      } 
+    },
+
+  maskify (creditCardNumber) {
+      let cardArray = creditCardNumber.split("")
+      let numbers = ""
+    
+    for (let i = 0; i < cardArray.length; i++)
+      if (i >= cardArray.length-4 ){
+        numbers += cardArray[i]
+      } else {
+        numbers += "#"        
+      }
+    return numbers;    
+    }   
 };
 
+
 export default validator;
+
+
